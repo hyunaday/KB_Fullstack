f1385557cb1bfcc4877277078536187625c19ab3
hyunaday <cyj9512@naver.com>
2024-06-28 17:30:20 +0900

    plz

--- 04_java/study/src/week1/test2/Solution.java
+++ 04_java/study/src/week1/test2/Solution.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class Solution {
     public int solution(int[] nums) {
         int counter = 0;
-
+ //안녕 2024.06.28
         for (int i = 0; i < nums.length - 2; i++) {
             for (int j = i + 1; j < nums.length - 1; j++) {
                 for (int k = j + 1; k < nums.length; k++) {
