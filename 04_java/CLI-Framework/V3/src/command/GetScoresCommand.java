41c93bec910ebf523c5ead7759663d564aedcff1
hyunaday <cyj9512@naver.com>
2024-07-03 19:51:11 +0900

    20240703_Lambda_Stream

---
+++ 04_java/CLI-Framework/V3/src/command/GetScoresCommand.java
@@ -0,0 +1,17 @@
+package command;
+
+import domain.StudentScores;
+import basic.Input;
+
+public class GetScoresCommand implements Command {
+    StudentScores studentScores = StudentScores.getInstance();
+
+    @Override
+    public void execute(){
+        int [] scores = studentScores.getScores();
+
+        for(int i = 0 ; i < scores.length ; i++){
+            scores[i] = Input.getInt("scores["+ i + "]> ");
+        }
+    }
+}
---
+++ 04_java/study/src/ch17/sec04/exam01/Product.java
@@ -0,0 +1,11 @@
+package ch17.sec04.exam01;
+
+@AllArgsConstructor
+@Data
+
+public class Product {
+    private int pno;
+    private String name;
+    private String company;
+    private int price;
+}
