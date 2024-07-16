393eec55b888e70a58a430801ebc7c3c23f9fc35
hyunaday <cyj9512@naver.com>
2024-07-16 17:52:08 +0900

    20240716_sql_dao

---
+++ 05_MySQL/jdbc_ex/src/main/java/org/scoula/jdbc_ex/dao/UserDao.java
@@ -0,0 +1,25 @@
+package org.scoula.jdbc_ex.dao;
+
+import org.scoula.jdbc_ex.domain.UserVO;
+
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Optional;
+
+public interface UserDao {
+    // 회원 등록 (CRUD 중 C)
+    int create(UserVO user) throws SQLException;
+
+    // 모든 사용자 목록 조회 (CRUD 중 R)
+    List<UserVO> getList() throws SQLException;
+
+    // 특정 사용자 정보 조회 (CRUD 중 R)
+    // 존재하지 않는 경우 빈 Optional 객체 반환
+    Optional<UserVO> get(String id) throws SQLException;
+
+    // 사용자 정보 수정 (CRUD 중 U)
+    int update(UserVO user) throws SQLException;
+
+    // 특정 사용자 삭제 (CRUD 중 D)
+    int delete(String id) throws SQLException;
+}
