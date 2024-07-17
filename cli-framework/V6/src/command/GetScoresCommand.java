57e2c40edcf9708a0b025f2aff38754f117215c2
hyunaday <cyj9512@naver.com>
2024-07-17 18:49:45 +0900

    20240717_CLI_V5,V6수정

---
+++ cli-framework/V6/src/command/GetScoresCommand.java
@@ -0,0 +1,18 @@
+package command;
+
+import basic.Input;
+import domain.StudentScores;
+
+public class GetScoresCommand implements Command {
+    StudentScores studentScores = StudentScores.getInstance();
+    @Override
+    public void execute() {
+        // 기존에 저장되어있는 학생들의 점수 리스트를 가져옴
+        int [] scores = studentScores.getScores();
+
+        // 점수 리스트를 돌면서 사용자한테 입력받은 점수를 넣어줌
+        for(int i = 0; i< scores.length; i++) {
+            scores[i] = Input.getInt("scores[" + i + "]> ");
+        }
+    }
+}
