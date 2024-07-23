67bab9aeeabfba55cf363669b6eb596909198344
hyunaday <cyj9512@naver.com>
2024-07-23 17:01:53 +0900

    20240723_수정_TodoApp,TodoDao

--- 04_java/정리/TodoApp/src/main/java/org/scoula/todo/TodoApp.java
+++ 04_java/정리/TodoApp/src/main/java/org/scoula/todo/TodoApp.java
@@ -3,18 +3,24 @@ package org.scoula.todo;
 import org.scoula.studentscore.lib.cli.App;
 import org.scoula.studentscore.lib.cli.ui.Menu;
 import org.scoula.studentscore.lib.cli.ui.MenuItem;
-import org.scoula.todo.command.*;
+import org.scoula.todo.service.ToDoService;
 
 public class TodoApp extends App {
+    // command를 모두 service 클래스 내에 메소드로 캡슐화
+    ToDoService service = new ToDoService();
+
     @Override
     public void createMenu(Menu menu) {
         super.createMenu(menu);
-        menu.add(new MenuItem("목록", new PrintTodoCommand()));
-        menu.add(new MenuItem("상세",  new DetailTodoCommand()));
-        menu.add(new MenuItem("추가", new AddTodoCommand()));
-        menu.add(new MenuItem("수정", new UpdateTodoCommand()));
-        menu.add(new MenuItem("삭제", new DeleteTodoCommand()));
+
+        // 메소드 참조를 통해 service 객체의 메소드 사용
+        menu.add(new MenuItem("목록", service::printTodoList));
+        menu.add(new MenuItem("상세", service::detailTodo));
+        menu.add(new MenuItem("추가", service::addTodo));
+        menu.add(new MenuItem("수정", service::updateTodo));
+        menu.add(new MenuItem("삭제", service::deleteTodo));
     }
+
     public static void main(String[] args) {
         App app = new TodoApp();
         app.run();
