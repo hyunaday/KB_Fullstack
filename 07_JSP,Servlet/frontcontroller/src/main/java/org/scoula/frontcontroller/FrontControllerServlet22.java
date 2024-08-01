c4877b104d6fca371c13477fd296340be8d9c42e
hyunaday <cyj9512@naver.com>
2024-08-01 19:30:18 +0900

    20240801_DispatcherServlet

--- 07_JSP,Servlet/frontcontroller/src/main/java/org/scoula/frontcontroller/FrontControllerServlet22.java
+++ 07_JSP,Servlet/frontcontroller/src/main/java/org/scoula/frontcontroller/FrontControllerServlet22.java
@@ -1,9 +1,12 @@
 package org.scoula.frontcontroller;
 
 import org.scoula.frontcontroller.command.Command;
+import org.scoula.frontcontroller.controller.HomeController;
+import org.scoula.frontcontroller.controller.TodoController;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -11,16 +14,22 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-import static jdk.vm.ci.code.RegisterAttributes.createMap;
-
-public class DispatcherServlet extends HttpServlet {
-
+//@WebServlet("/")
+//경로 지정하는 방법은 두가지가 있다, 1) 디렉토리 기반, 2) 확장명 기반
+//디렉토리 기반(/board), 확장명 기반(*.do)
+@WebServlet(name="frontControllerServlet", value="/")
+public class FrontControllerServlet22 extends HttpServlet {
     Map<String, Command> getMap; // GET 요청이 들어왔을 경우 사용할 맵
     Map<String, Command> postMap; // POST 요청이 들어왔을 경우 사용할 맵
 
     // 뷰의 기본 경로와 확장자를 설정해준다
     String prefix = "/WEB-INF/views/";
     String suffix = ".jsp";
+
+    // HomeController 객체 생성
+    HomeController homeController = new HomeController();
+    // TodoController 객체 생성
+    TodoController todoController = new TodoController();
 
     // 서블릿 초기화 메소드
     @Override
@@ -29,9 +38,28 @@ public class DispatcherServlet extends HttpServlet {
         getMap = new HashMap<>();
         postMap = new HashMap<>();
 
-        createMap(getMap,postMap);
-}
-    protected void createMap(Map<String, Command> map, Map<String, Command> postMap) {}
+//        "/"경로로 접근하면 HomeController의 getIndex 메소드를 찾을 수 있다. (메소드 참조)
+//        put으로 Map에 아이템을 넣어준다
+        getMap.put("/", homeController::getIndex);
+
+        getMap.put("/todo/list", todoController::getList);
+                getMap.put("/todo/view", todoController::getView);
+        getMap.put("/todo/update", todoController::getUpdate);
+        getMap.put("/todo/create", todoController::getCreate);
+
+        postMap.put("/todo/delete", todoController::postDelete);
+        postMap.put("/todo/update", todoController::postUpdate);
+        postMap.put("/todo/create", todoController::postCreate);
+
+        // 실제 경로
+//        getMap.put("/", new Command() {
+//            @Override
+//            public String execute(HttpServletRequest request, HttpServletResponse response) throws IOException {
+//                return new HomeController().getIndex(request, response);
+//            }
+//        });
+
+    }
 
     public void execute(Command command, HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         // 실행 결과는 jsp의 이름으로 받아온다.
@@ -46,6 +74,7 @@ public class DispatcherServlet extends HttpServlet {
             RequestDispatcher dis = request.getRequestDispatcher(view);
             dis.forward(request, response);
         }
+
     }
     // GET 요청을 처리해주는 메소드
     @Override
--- 07_JSP,Servlet/frontcontroller/src/main/webapp/WEB-INF/views/todo/create.jsp
+++ 07_JSP,Servlet/frontcontroller/src/main/webapp/WEB-INF/views/todo/create.jsp
@@ -8,7 +8,7 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <html>
-<head>
+<head>ㅇ
     <title>Title</title>
 </head>
 <body>
