5ebaeebefbbd0c69f2ba3e6780985140893d4df7
hyunaday <cyj9512@naver.com>
2024-08-20 17:50:59 +0900

    20240820_로그인과 로그아웃/member/UserDetails

---
+++ 08_Spring/secserver/src/main/java/org/scoula/security/account/domain/AuthVO.java
@@ -0,0 +1,17 @@
+package org.scoula.security.account.domain;
+
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+
+//member_auth 테이블과 매칭되는 VO
+@Data
+public class AuthVO implements GrantedAuthority {
+    private String username;
+    private String auth;
+
+    @Override
+    public String getAuthority() {
+//        권한정보만 추출해주는 메소드
+        return auth;
+    }
+}
