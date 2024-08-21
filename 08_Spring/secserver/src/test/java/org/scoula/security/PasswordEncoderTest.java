d6d80a533be2aff431f8cb2de4a1e9b14e015544
hyunaday <cyj9512@naver.com>
2024-08-21 14:03:47 +0900

    20240821

--- 08_Spring/secserver/src/test/java/org/scoula/security/PasswordEncoderTest.java
+++ 08_Spring/secserver/src/test/java/org/scoula/security/PasswordEncoderTest.java
@@ -21,20 +21,22 @@ public class PasswordEncoderTest {
     private PasswordEncoder pwEncoder;
 
     @Test
-    public void testEncode() {
+    public void testEncode(){
         String str = "1234";
 
         // 비밀번호 인코딩 및 로그 출력
-        String enStr = pwEncoder.encode(str); // 암호화
+        String enStr = pwEncoder.encode(str);
         log.info("password: " + enStr);
 
         // 동일한 비밀번호 다시 인코딩
-        String enStr2 = pwEncoder.encode(str); // 암호화
+        String enStr2 = pwEncoder.encode(str);
         log.info("password: " + enStr2);
-        // 인코딩된 비밀번호와 원본 비밀번호를 비교할 때는 matches 사용
-        log.info("match :" + pwEncoder.matches(str, enStr)); // 비밀번호 일치 여부 검사
-        log.info("match :" + pwEncoder.matches(str, enStr2)); // 비밀번호 일치 여부 검사
-        // 각각의 인코딩된 비밀번호를 비교했을 때는 다르다고 나온다
-        log.info("match :" + pwEncoder.matches(enStr, enStr2)); // 비밀번호 일치 여부 검사
+
+//        인코딩된 비밀번호와 원본 비밀번호를 비교할 때는 matches 사용
+        log.info("match : "+pwEncoder.matches(str, enStr));
+        log.info("match : "+pwEncoder.matches(str, enStr2));
+//        각각의 인코딩된 비밀번호를 비교했을 때는 다르다고 나온다 ..
+        log.info("match : "+pwEncoder.matches(enStr, enStr2));
+
     }
 }
