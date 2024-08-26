70988cfe0b9ab654db35b23de8724b12ba908e25
hyunaday <cyj9512@naver.com>
2024-08-26 14:36:01 +0900

    20240826

--- 04_java/ScoulaLib/scoulacli/src/main/java/org/scoula/studentscore/lib/cli/command/ExitCommand.java
+++ 04_java/ScoulaLib/scoulacli/src/main/java/org/scoula/studentscore/lib/cli/command/ExitCommand.java
@@ -1,10 +1,3 @@
-package org.scoula.studentscore.lib.cli.command;
-
-// 종료 기능 처리하는 클래스
-public class ExitCommand implements Command {
-    @Override
-    public void execute() {
-        System.out.println("프로그램 종료");
-        System.exit(0);
-    }
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:3d29cfe095994dc260f287f598e95ac067b5b466227c9eb06a0b4fe3e59af272
+size 261
