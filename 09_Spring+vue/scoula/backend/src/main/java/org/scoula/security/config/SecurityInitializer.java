011cdd227bcc664cc15399cbb7d503bfb5b66620
hyunaday <cyj9512@naver.com>
2024-08-27 17:36:55 +0900

    20240827_new

--- 09_Spring+vue/scoula/backend/src/main/java/org/scoula/security/config/SecurityInitializer.java
+++ 09_Spring+vue/scoula/backend/src/main/java/org/scoula/security/config/SecurityInitializer.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9773332fccf77e6fcdd716ed071196d999acd075d89cfd9a981e1814c118f08e
-size 216
+oid sha256:57e3d9828d0cf281274f0e4574bff1a8bd87fd714b7c39e9590c2473c561c1b5
+size 280
